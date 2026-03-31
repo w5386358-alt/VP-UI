b48d9a0f504b525e8af3d3d92aee5ede84dd652d
w5386358-alt <w5386358@gmail.com>
2026-03-31 14:56:44 +0800

    Update App.tsx

--- vp-ui-package/src/App.tsx
+++ vp-ui-package/src/App.tsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import {addDoc,collection,serverTimestamp} from "firebase/firestore";
-import {db} from "./firebase";
+import {db} from "./firebase.ts";
 
 export default function App(){
 
