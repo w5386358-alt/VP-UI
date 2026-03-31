eba3c9487a033f95330f75f69ad9647463b34902
w5386358-alt <w5386358@gmail.com>
2026-03-31 15:03:05 +0800

    Update firebase.ts

--- vp-ui-package/src/firebase.ts
+++ vp-ui-package/src/firebase.ts
@@ -1,9 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  // 填你的
-};
+let db: any = null;
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+try {
+  const firebaseConfig = {
+    apiKey: "x", // 隨便填也可以先跑UI
+    authDomain: "x",
+    projectId: "x",
+    appId: "x"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  db = getFirestore(app);
+
+} catch (e) {
+  console.log("Firebase 初始化失敗，但UI繼續跑");
+}
+
+export { db };
